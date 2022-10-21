d90bea4f114d39982bd2dbee58e78a26ffc8db77
fadhlu <fadhlurahman.i@gmail.com>
2022-10-21 15:59:05 +0800

    add vercel and meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Cards from "../components/Cards";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Hourly Absurd</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div className="container mx-auto my-8 px-4 md:px-8">
         <Cards />
       </div>
